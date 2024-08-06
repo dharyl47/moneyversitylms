333dd26ef67813c727060633c009877be644995d
dharyl47 <70868109+dharyl47@users.noreply.github.com>
2024-08-06 17:28:29 +0800

    added env

--- src/app/lib/mongo.js
+++ src/app/lib/mongo.js
@@ -1,12 +1,15 @@
-
 import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
-    try{
-        await mongoose.connect("mongodb+srv://blacandbloo:jnijdnsfnjMLKNDVJKSfdfs3434@moneyversityai.ut7dw3f.mongodb.net/Moneyversity?retryWrites=true&w=majority&appName=MoneyversityAI");
-          console.log("Connected to MongoDB")
-    } catch (Error) {
-        console.log("Error")
+    try {
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error("MongoDB URI is not defined in environment variables.");
+        }
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error:", error.message);
     }
 };
 
