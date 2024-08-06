2357bb23c1835fa46734c59298ba0d8ef6fc2118
eiroljan <eiroljan19@gmail.com>
2024-08-06 17:59:24 +0800

    fix image

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -260,7 +260,11 @@ const Navbar = () => {
               data-dropdown-toggle="dropdown"
             >
               <span className="sr-only">Open user menu</span>
-              <Image src="/images/users/neil-sims.png" alt="user photo" width={32} height={32} className="w-8 h-8 rounded-full" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+              {/* <Image src="/images/users/neil-sims.png" alt="user photo" width={32} height={32} className="w-8 h-8 rounded-full" /> */}
             </button>
             <div
               className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:divide-gray-600 dark:bg-gray-700"
