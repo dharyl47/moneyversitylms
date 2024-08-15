14477fb53f5083e78b79ec6ecd5fb0f7889f4a39
eiroljan <eiroljan19@gmail.com>
2024-08-15 16:03:36 +0800

    update

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -7,20 +7,20 @@ export default function Privacy() {
         <div className="p-6 max-w-6xl mx-auto">
           <h1 className="text-5xl font-bold mb-4 text-center">Privacy Policy</h1>
           <p className="text-base leading-relaxed">
-            The companies in the Old Mutual Group represented on this website (“Old Mutual Moneyversity”) and its employees respect your privacy and the confidentiality of your personal information. Personal information that you supply to us via this website and via e­mail (“your personal information”) is only used and processed for the purpose: For which we request the information, as stated in the relevant e-mail; and/or to the extent necessary to enable Old Mutual to meet its obligations to you; and to comply with our legal obligations.
-            <br/><br/>
+            The companies in the Old Mutual Group represented on this website (&ldquo;Old Mutual Moneyversity&rdquo;) and its employees respect your privacy and the confidentiality of your personal information. Personal information that you supply to us via this website and via e­mail (&ldquo;your personal information&rdquo;) is only used and processed for the purpose: For which we request the information, as stated in the relevant e-mail; and/or to the extent necessary to enable Old Mutual to meet its obligations to you; and to comply with our legal obligations.
+            <br /><br />
             Old Mutual will not disclose your personal information to third parties outside the Old Mutual Group, unless: Old Mutual is permitted or required to do so by law; or it is in the public interest to do so; or it is necessary to protect the rights of a company in the Old Mutual Group; or you have expressly authorised Old Mutual to do so.
-            <br/><br/>
+            <br /><br />
             Old Mutual may from time to time use your personal information to compile profiles for statistical purposes. However, such profiles or statistical data cannot be linked to any identifiable individual. Old Mutual has put controls in place to prevent unauthorised access to, modification or destruction of your personal information, whether accidental or deliberate. These controls are based on the requirements of the business and conform to commonly accepted industry standards as well as contractual and legal requirements.
-            <br/><br/>
-            The various companies in the Old Mutual Group may have different online privacy policies due to the fact that the nature of their services or products require a deviation from this general policy. If this is the case, the relevant company's specific policy will govern your use of its web site, on-line services and products.
-            <br/><br/>
+            <br /><br />
+            The various companies in the Old Mutual Group may have different online privacy policies due to the fact that the nature of their services or products require a deviation from this general policy. If this is the case, the relevant company&rsquo;s specific policy will govern your use of its web site, on-line services and products.
+            <br /><br />
             If you do not interact with us or if you browse our website anonymously, we may from time to time collect information such as your email address. We may occasionally contact you to inform you of new services or products offered by a company in the Old Mutual Group. You will, however, always be given the opportunity to instruct us to remove your details from our mailing list.
-            <br/><br/>
-            Old Mutual reserves the right to amend this online privacy policy at any time by posting the amended policy on our website. Unless otherwise stated, the current version will apply each time you access our website." Copyright subsists in all materials on this website. You may use the information and print or reproduce materials from this website only.
+            <br /><br />
+            Old Mutual reserves the right to amend this online privacy policy at any time by posting the amended policy on our website. Unless otherwise stated, the current version will apply each time you access our website.&rdquo; Copyright subsists in all materials on this website. You may use the information and print or reproduce materials from this website only.
           </p>
         </div>
       </Layout>
-      </main>
+    </main>
   );
 }
