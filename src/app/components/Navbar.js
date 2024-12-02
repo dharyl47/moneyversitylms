ffa133bd36fccac54a0322e81106551b615775b5
Dharyl Jerson Bajenting <dharyljersonbajenting11@gmail.com>
2024-12-02 18:16:07 +0800

    updates

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -51,45 +51,7 @@ const Navbar = () => {
       {/* Right Side Icons */}
       <div className="absolute right-4 flex items-center space-x-3">
         {/* Search Bar */}
-        <div className="hidden lg:block lg:w-96">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search"
-              className="w-full pl-10 p-2.5 bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            />
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-          </div>
-        </div>
-
-        {/* Notifications Icon */}
-        <button
-          type="button"
-          className="p-2 text-gray-500 rounded-lg hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
-        >
-          <span className="sr-only">View notifications</span>
-          <svg
-            className="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
-          </svg>
-        </button>
+       
       </div>
     </div>
   </div>
